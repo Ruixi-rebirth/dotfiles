fadcab6569acd629508dd18a36e83483b5850bb4
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-01-29 10:37:00 +0800

    emm

--- ranger/commands.py
+++ ranger/commands.py
@@ -98,42 +98,40 @@ class compress(Command):
         return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
 
 
-    #extract
-    import os
+#extract
+import os
 from ranger.core.loader import CommandLoader
 
-class extracthere(Command):
+class extract_here(Command):
     def execute(self):
-        """ Extract copied files to current directory """
-        copied_files = tuple(self.fm.copy_buffer)
-
-        if not copied_files:
-            return
+        """ extract selected files to current directory."""
+        cwd = self.fm.thisdir
+        marked_files = tuple(cwd.get_selection())
 
         def refresh(_):
             cwd = self.fm.get_directory(original_path)
             cwd.load_content()
 
-        one_file = copied_files[0]
+        one_file = marked_files[0]
         cwd = self.fm.thisdir
         original_path = cwd.path
-        au_flags = ['-X', cwd.path]
+        au_flags = ['-x', cwd.path]
         au_flags += self.line.split()[1:]
         au_flags += ['-e']
 
         self.fm.copy_buffer.clear()
         self.fm.cut_buffer = False
-        if len(copied_files) == 1:
+        if len(marked_files) == 1:
             descr = "extracting: " + os.path.basename(one_file.path)
         else:
-            descr = "extracting files from: " + os.path.basename(one_file.dirname)
-        obj = CommandLoader(args=['aunpack'] + au_flags \
-                + [f.path for f in copied_files], descr=descr)
+            descr = "extracting files from: " + os.path.basename(
+                one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags
+                            + [f.path for f in marked_files], descr=descr,
+                            read=True)
 
         obj.signal_bind('after', refresh)
         self.fm.loader.add(obj)
-
-
 
 
 ###################
@@ -170,106 +168,5 @@ class mkcd(Command):
             self.fm.notify("file/directory exists!", bad=True)
 
 
-
-###############
-#upload files
-class up(Command):
-    def execute(self):
-        if self.arg(1):
-            scpcmd = ["scp", "-r"]
-            scpcmd.extend([f.realpath for f in self.fm.thistab.get_selection()])
-            scpcmd.append(self.arg(1))
-            self.fm.execute_command(scpcmd)
-            self.fm.notify("Uploaded!")
-
-
-    def tab(self, tabnum):
-        import os.path
-        try:
-            import paramiko
-        except ImportError:
-            """paramiko not installed"""
-            return
-
-        try:
-            with open(os.path.expanduser("~/.ssh/config")) as file:
-                paraconf = paramiko.SSHConfig()
-                paraconf.parse(file)
-        except IOError:
-            """cant open ssh config"""
-            return
-
-        hosts = sorted(list(paraconf.get_hostnames()))
-        # remove any wildcard host settings since they're not real servers
-        hosts.remove("*")
-        query = self.arg(1) or ''
-        matching_hosts = []
-        for host in hosts:
-            if host.startswith(query):
-                matching_hosts.append(host)
-        return (self.start(1) + host + ":" for host in matching_hosts)
-
-
-##############
-#fzf integration
-class fzf_select(Command):
-    """
-    :fzf_select
-    Find a file using fzf.
-    With a prefix argument to select only directories.
-
-    See: https://github.com/junegunn/fzf
-    """
-
-    def execute(self):
-        import subprocess
-        import os
-        from ranger.ext.get_executables import get_executables
-
-        if 'fzf' not in get_executables():
-            self.fm.notify('Could not find fzf in the PATH.', bad=True)
-            return
-
-        fd = None
-        if 'fdfind' in get_executables():
-            fd = 'fdfind'
-        elif 'fd' in get_executables():
-            fd = 'fd'
-
-        if fd is not None:
-            hidden = ('--hidden' if self.fm.settings.show_hidden else '')
-            exclude = "--no-ignore-vcs --exclude '.git' --exclude '*.py[co]' --exclude '__pycache__'"
-            only_directories = ('--type directory' if self.quantifier else '')
-            fzf_default_command = '{} --follow {} {} {} --color=always'.format(
-                fd, hidden, exclude, only_directories
-            )
-        else:
-            hidden = ('-false' if self.fm.settings.show_hidden else r"-path '*/\.*' -prune")
-            exclude = r"\( -name '\.git' -o -iname '\.*py[co]' -o -fstype 'dev' -o -fstype 'proc' \) -prune"
-            only_directories = ('-type d' if self.quantifier else '')
-            fzf_default_command = 'find -L . -mindepth 1 {} -o {} -o {} -print | cut -b3-'.format(
-                hidden, exclude, only_directories
-            )
-
-        env = os.environ.copy()
-        env['FZF_DEFAULT_COMMAND'] = fzf_default_command
-        env['FZF_DEFAULT_OPTS'] = '--height=40% --layout=reverse --ansi --preview="{}"'.format('''
-            (
-                batcat --color=always {} ||
-                bat --color=always {} ||
-                cat {} ||
-                tree -ahpCL 3 -I '.git' -I '*.py[co]' -I '__pycache__' {}
-            ) 2>/dev/null | head -n 100
-        ''')
-
-        fzf = self.fm.execute_command('fzf --no-multi', env=env,
-                                      universal_newlines=True, stdout=subprocess.PIPE)
-        stdout, _ = fzf.communicate()
-        if fzf.returncode == 0:
-            selected = os.path.abspath(stdout.strip())
-            if os.path.isdir(selected):
-                self.fm.cd(selected)
-            else:
-                self.fm.select_file(selected)
 
 
